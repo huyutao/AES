acd2ed260fc7e3050eea6a191ac665367a300d5f
Yutao Hu <yutaohu1024@gmail.com>
2017-03-19 22:38:03 -0400

    Make the round key
    making the key and calculate the following round keys

---
+++ python AES/test.py
@@ -0,0 +1,34 @@
+from AES import AES
+import numpy as np
+
+a = AES(0x2b7e151628aed2a6abf7158809cf4f3c)
+a.print_key()
+
+
+#### round const
+# round_const = [0x00,0x01]
+# a = 0x01
+# for i in range(30):
+#     a = a*0x02
+#     if a > 0xff:
+#         a = a^0b100011011
+#     round_const.append(a)
+# np.set_printoptions(formatter={'int': hex})
+# print(np.array(round_const))
+
+
+# array = [[0 for x in range(4)] for y in range(4)]
+# print(array)
+# block = range(128)
+# print(block)
+#
+# for i in range(4):
+#     for j in range(4):
+#         print(block[32 * i + 8 * j : 32 * i + 8 * (j+1)])
+#         array[j][i] = block[32 * i + 8 * j : 32*i + 8*(j+1)]
+#     print(" ")
+#
+# print(" ")
+# for i in range(4):
+#     for j in range(4):
+#         print(array[i][j])
